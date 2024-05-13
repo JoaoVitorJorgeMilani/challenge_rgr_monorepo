36f822b71ce7cc3eaa78c39d462c4dd37b5ae430
JoaoVitorJorgeMilani <joaovjmilani@gmail.com>
2024-05-13 20:57:49 -0300

    refact datepick

--- web-transfer/src/app/shared/datepicker/datepicker.component.ts
+++ web-transfer/src/app/shared/datepicker/datepicker.component.ts
@@ -1,20 +1,21 @@
 import { formatDate } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild, forwardRef } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, forwardRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => DatepickerComponent),
-      multi: true
-    }
-  ],  
   selector: 'app-datepicker',
   template: `
     <div style="padding: 10px 10px 10px 0px">
       <label [for]='id' class="mb-1">{{labelText}}</label>
-      <input #datepickerField id="startDate" class="form-control" type="date" (input)="onInput($event)" [min]='minx' />
+      <input 
+        id="startDate" 
+        class="form-control" 
+        type="date" 
+        [min]='minValue' 
+        [max]='maxValue' 
+        [(ngModel)]="value"
+        (ngModelChange)="onModelChange($event)" 
+      />
     </div>
   `
 })
@@ -22,36 +23,39 @@ export class DatepickerComponent {
 
   @ViewChild('datepickerField') datepickerField!: ElementRef;
   
+  @Input() value: string = '';
+
   @Input() labelText: string = '';
   @Input() id: string = '';
   @Input() name: string = '';
   @Input() disabled: boolean = false;
-  @Input() minDate: string = '1970-01-01';
+  @Input() min: string = '';
+  @Input() max: string = '';
   @Input() startsToday: boolean = false;
+  @Input() endsToday: boolean = false;
 
-  private onChange!: (value: any) => void;
+  @Output() valueChange = new EventEmitter<any>();
 
-  onInput(event: any): void {
-    this.onChange(event.target.value);
+  onModelChange(event: any): void {
+    console.log("MODEL CHANGED");
+    console.log(event);
+    this.valueChange.emit(event);
   }
 
   writeValue(value: any): void {
     this.datepickerField.nativeElement.value = value;
   }
 
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-  }
-
   setDisabledState(isDisabled: boolean): void {
     this.datepickerField.nativeElement.disabled = isDisabled;
   }
 
-  get minx () : string {
-    return this.startsToday ? formatDate(new Date(), 'yyyy-MM-dd', 'pt-BR') : formatDate(this.minDate, 'yyyy-MM-dd', 'pt-BR')
+  get minValue () : string {
+    return this.startsToday ? formatDate(new Date(), 'yyyy-MM-dd', 'pt-BR') : this.min ?  formatDate(this.min, 'yyyy-MM-dd', 'pt-BR') : '';
+  }
+
+  get maxValue () : string {
+    return this.endsToday ? formatDate(new Date(), 'yyyy-MM-dd', 'pt-BR') : this.max ?  formatDate(this.max, 'yyyy-MM-dd', 'pt-BR') : '';
   }
 
 
