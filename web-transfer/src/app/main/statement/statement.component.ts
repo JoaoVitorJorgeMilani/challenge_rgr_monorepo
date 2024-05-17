2fc4783826e46bddd678c1edd7b9579808d46cd9
JoaoVitorJorgeMilani <joaovjmilani@gmail.com>
2024-05-17 18:17:38 -0300

    added table on statement for tests

--- web-transfer/src/app/main/statement/statement.component.ts
+++ web-transfer/src/app/main/statement/statement.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class StatementComponent {
 
+
+  schedules = [
+    {
+      source: '123',
+      destination: '456',
+      amount: 100
+    },
+    {
+      source: '12345456',
+      destination: '45654654',
+      amount: 101
+    }
+  ]
+
+
+  onViewSchedule(item: any) {
+    console.log(item);
+  }
 }
