9d3704c05816605f2ec92c5cce34fd3fd8956531
JoaoVitorJorgeMilani <joaovjmilani@gmail.com>
2024-05-18 13:41:03 -0300

    added UUID encrypt decrypt tests

--- web-transfer-back/src/test/java/com/rgr/webtransferback/util/encryption/AESCryptTest.java
+++ web-transfer-back/src/test/java/com/rgr/webtransferback/util/encryption/AESCryptTest.java
@@ -2,14 +2,21 @@ package com.rgr.webtransferback.util.encryption;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
@@ -72,5 +79,35 @@ public class AESCryptTest {
                 "QWERTYUIOPASDFGHJKLZXCVBNM1234",
                 "!@#$%^&*()-_=+[{]}|;:',<.>/?"
         );
+    }
+
+    @Test
+    void encryptUuid_ShouldEncryptUuid() {
+        UUID uuid = UUID.randomUUID();
+        
+        String encryptedUuid = aesCrypt.encryptUuid(uuid);
+
+        assertNotNull(encryptedUuid);
+        assertThat(encryptedUuid.length(), greaterThan(0));
+    }
+
+    @Test
+    void decryptUuid_ShouldDecryptUuid() {
+        UUID uuid = UUID.randomUUID();
+        String encryptedUuid = aesCrypt.encryptUuid(uuid);
+
+        UUID decryptedUuid = aesCrypt.decryptUuid(encryptedUuid);
+
+        assertNotNull(decryptedUuid);
+        assertEquals(uuid, decryptedUuid);
+    }
+
+    @Test
+    void encrypt_ShouldThrowsIllegalArgumentException() {
+        String invalidUuidStr = "lakjsdçflkjasçdkljfçlkajsdf";
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> aesCrypt.decryptUuid(invalidUuidStr));
+
+        assertThat(exception, instanceOf(IllegalArgumentException.class));        
     }
 }
